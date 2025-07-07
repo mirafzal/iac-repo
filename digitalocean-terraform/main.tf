801d677b3333b8371a2aa820cfe562b1be1b7225
= <shavkatovm99@gmail.com>
2025-07-07 21:28:49 +0500

    add graph, ssh key, load balancer

--- digitalocean-terraform/main.tf
+++ digitalocean-terraform/main.tf
@@ -1,4 +1,9 @@
-# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_vpc" "app-server-vpc" {
+  name     = "app-server-vpc"
+  region   = "ams3"
+  ip_range = "10.10.10.0/24"
+}
+
 resource "digitalocean_droplet" "app-server" {
   for_each = var.droplets
 
@@ -6,11 +11,36 @@ resource "digitalocean_droplet" "app-server" {
   name    = each.value.name
   region  = each.value.region
   size    = each.value.size
+  ssh_keys = [digitalocean_ssh_key.default-ssh-key.id]
   vpc_uuid = each.value.region == "ams3" && each.value.vpc_uuid == null ? digitalocean_vpc.app-server-vpc.id : each.value.vpc_uuid
 }
 
-resource "digitalocean_vpc" "app-server-vpc" {
-  name     = "app-server-vpc"
-  region   = "ams3"
-  ip_range = "10.10.10.0/24"
+resource "digitalocean_ssh_key" "default-ssh-key" {
+  name       = "default-ssh-key"
+  public_key = file("./files/id_rsa.pub")
+}
+
+resource "digitalocean_loadbalancer" "app-server-lb" {
+  name   = "app-server-lb"
+  region = "ams3"
+
+  vpc_uuid = digitalocean_vpc.app-server-vpc.id
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 80
+    protocol = "tcp"
+  }
+
+  droplet_ids = [
+    for d in digitalocean_droplet.app-server :
+    d.id if d.region == "ams3" && d.vpc_uuid == digitalocean_vpc.app-server-vpc.id
+  ]
 }
